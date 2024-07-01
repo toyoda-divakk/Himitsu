70604b3ad4847e2d06fc03d91476cc939972fa84
toyoda <toyoda@divakk.co.jp>
2024-07-02 08:29:12 +0900

    Works

--- Himitsu/Program.cs
+++ Himitsu/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleAppFramework;
+using Himitsu.Commands;
 // ビルド後イベントを設定しているので、ビルドしたら"E:\Program Files\Himitsu"に自動でコピーします。
 // 環境変数のPathに"E:\Program Files\Himitsu"を追加する。
 
@@ -6,13 +7,13 @@
 
 var app = ConsoleApp.Create();
 app.Add<MyCommands>();
+app.Add<Works>();
 app.Run(args);
 
 public class MyCommands // IDisposable, IAsyncDisposableがついていたら、コマンド実行後にDisposeされる。
 {
     /// <summary>Root command test.</summary>
     /// <param name="msg">-m, Message to show.</param>
-    [Command("")]
     public void Root(string msg) => Console.WriteLine(msg);     // ※Command属性を使用して名前を任意に変更することもできる。メソッド名が長くなり過ぎた時なんかに。
 
     /// <summary>Display message.</summary>
