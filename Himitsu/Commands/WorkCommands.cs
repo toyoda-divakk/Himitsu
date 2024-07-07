b53c996200b4febd8e2c072ba4c51876cd1236b0
genkokudo <y-toyoda@live.jp>
2024-07-07 18:52:55 +0900

    3日以上のタスク集約に対応

--- Himitsu/Commands/WorkCommands.cs
+++ Himitsu/Commands/WorkCommands.cs
@@ -2,6 +2,7 @@
 using ConsoleAppFramework;
 using System.Text;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Himitsu.Commands
 {
@@ -30,6 +31,51 @@ namespace Himitsu.Commands
             var res = await client.PostAsync(url, content);
             return await res.Content.ReadAsStringAsync();
         }
+
+        /// <summary>
+        /// 日付だけでDistinctを取って、稼働日数を算出する。
+        /// </summary>
+        /// <param name="records"></param>
+        /// <returns></returns>
+        private static int GetWorkDays(List<WorkRecord> records) => records.Select(x => x.Date).Select(y => y.Day).Distinct().Count();
+
+        /// <summary>
+        /// 工数データの中で、連続した日付で同じタスクを行った場合、期間をまとめて表示します。
+        /// </summary>
+        /// <param name="records">月次工数データ</param>
+        /// <returns>集約済み工数データ</returns>
+        private List<WorkRecord> AggregateWorkRecords(List<WorkRecord> records)
+        {
+            if (records.Count == 0)
+            {
+                return records;
+            }
+            records.Sort((x, y) => x.TaskName.CompareTo(y.TaskName));
+            records.Sort((x, y) => x.Date.CompareTo(y.Date));
+
+            var result = new List<WorkRecord>();
+            var temp = records.First();
+            var days = 1;
+            for (int i = 1; i < records.Count; i++)
+            {
+                var next = records[i];
+                if (temp.TaskName == next.TaskName && temp.Date.Day + days == next.Date.Day)
+                {
+                    temp.EndDate = next.Date;
+                    temp.Hours += next.Hours;
+                    days++;
+                }
+                else
+                {
+                    result.Add(temp);
+                    temp = records[i];
+                    days = 1;
+                }
+            }
+            result.Add(temp);
+            return result;
+        }
+
         #endregion
 
         /// <summary>
@@ -39,7 +85,7 @@ namespace Himitsu.Commands
         /// <param name="hours">-h, 工数</param>
         /// <param name="date">-d, 日付：yyMMdd</param>
         [Command("work rec")]
-        public async Task RecordWorkAsync(string name, double hours = 7.75, string? date = null)        // asyncの時はvoidにしてしまうと待機せずにメソッドが終了してしまい、Postが終わる前にプログラムが終了してしまう。
+        public async Task RecordWorkAsync(string name, double hours = 7.75, string? date = null)
         {
             // データの準備
             DateTime? dt = date == null ? null : DateTime.ParseExact(date, "yyMMdd", null);
@@ -54,9 +100,10 @@ namespace Himitsu.Commands
         /// 指定した月の工数を取得して、書類フォーマットで出力します。
         /// </summary>
         /// <param name="month">-m, 年月yyyyMM</param>
+        /// <param name="test">-t, 纏めないで出力</param>
         /// <returns></returns>
         [Command("work get")]
-        public async Task GetWorkAsync(string? month = null)
+        public async Task GetWorkAsync(string? month = null, bool test = false)
         {
             var partitionKey = month ?? DateTime.Now.ToString("yyyyMM");
             using var client = new HttpClient();
@@ -68,10 +115,18 @@ namespace Himitsu.Commands
                 Console.WriteLine("工数の取得ができませんでした。");
                 return;
             }
+            var standup = GetWorkDays(records) * 0.25;
+            Console.WriteLine($"日次\tCRX Daily Standup\t{standup}h");
+
+            if (!test)
+            {
+                records = AggregateWorkRecords(records);
+            }
             foreach (var record in records)
             {
                 Console.WriteLine(record.ToSheetFormat());
             }
+            Console.WriteLine($"\t合計作業時間(概算)\t{records.Sum(x => x.Hours) + standup}h");
         }
 
         /// <summary>
@@ -88,5 +143,6 @@ namespace Himitsu.Commands
             var res = await client.DeleteAsync(url);
             Console.WriteLine(await res.Content.ReadAsStringAsync());
         }
+
     }
 }
